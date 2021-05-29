bf29af28474f6976a86cc8ec68bcef7ba51a3b16
keshikaa <keshikaa11@gmail.com>
2021-05-29 19:11:18 +0530

    cssdoing

--- clubadmin.php
+++ clubadmin.php
@@ -8,49 +8,21 @@ if(isset($_SESSION['login']) == 2)
     include "db.php";
     $club = $_SESSION['club'];
     $login = $_SESSION['login'];
-    if($_SESSION['login'] == 2){
-      $admin = 1;
-    }
-    else{
-      $admin = 0;
-    }
     $query = "SELECT * FROM announcements WHERE club_id = '$club'";
     $result = mysqli_query($con,$query);
 ?>
 <head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="Images/logohat.jpeg" type="image/icon type">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel = "stylesheet" href = "announcement.css">
+<link rel="icon" href="favicon.ico" type="image/icon type">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    
-    
-    <!--CSS Style-->
-    <link rel = "stylesheet" href = "announcement.css">
-    
-    <!--Fonts-->
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-      integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
-      crossorigin="anonymous"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
-      rel="stylesheet"
-    />
+  <!--links-->
+  
 <title>Uniclub | Club Admin</title>
 </head>
 <body>
@@ -97,7 +69,7 @@ if(isset($_SESSION['login']) == 2)
   <!--navbar ends-->
 
 
-    <h1 style ="text-align:center;margin-top:50px;">Announcements</h1>
+    <h1 style ="text-align:center">Announcements</h1>
     <div class="container">
     <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Add announcement</button>
 
@@ -109,9 +81,7 @@ if(isset($_SESSION['login']) == 2)
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <!--model 1 button-->
-          <span style="float:left"> <h4 class="modal-title">Add announcement</h4>
-          
+          <h4 class="modal-title">Add announcement</h4>
         </div>
         <div class="modal-body">
         <form>
@@ -141,8 +111,7 @@ if(isset($_SESSION['login']) == 2)
   </div>
 </div>
 
-<!--model 2 button-->
-<span style="float:left"><button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Add document</button></span>
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Add document</button>
 
 
 <div class="modal fade" id="myModal2" role="dialog">
@@ -171,7 +140,7 @@ if(isset($_SESSION['login']) == 2)
   </div>
 </div>
 
-<div class="container" style = "border-style: groove;border: 3px solid #495d87;margin-bottom:400px;margin-top:50px;">
+<div class="container" style = "border-style: groove;border: 3px solid #495d87;">
   <table class="table table-hover">
   <thead>
   <br>
@@ -204,13 +173,12 @@ if(isset($_SESSION['login']) == 2)
     ?>
   </tbody>
 </table>
-  </div>
-  
+
 </body>
 </html>
+
 <?php
 mysqli_close($con);
-include "footer.php";
 }
 else
 {
@@ -220,7 +188,6 @@ else
     echo "</script>";
 
 }?>
-
 <script>
 function update(id){
     var club_id = $("#club_id"+id).val();
@@ -289,6 +256,4 @@ function add(){
     location.reload();
 }
 </script>
-
-
 
