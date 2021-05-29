15d6ae2bc1da628803352b6b3cd4609c5280784d
akshayar0811 <akshayar0811@gmail.com>
2021-05-29 12:57:53 +0530

    Changed footer

--- footer.php
+++ footer.php
@@ -16,49 +16,10 @@
    <!-- Footer -->
 <footer class="page-footer font-small unique-color-dark">
 
-<div style="background-color: #b8b5ff;">
+<div style="background-color: #b8b5ff; height: 45px;">
   <div class="container" id ="contact">
-
-    <!-- Grid row-->
-    <div class="row py-4 d-flex align-items-center">
-
-      <!-- Grid column -->
-      <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
-        <h6 style="font-family:verdana;">Get connected with us on social networks!</h6>
-      </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-      <div class="col-md-6 col-lg-7 text-center text-md-right">
-
-        <!-- Facebook -->
-        <a class="fb-ic">
-            <i class="fab fa-facebook-f white-text mr-4"> </i>
-          </a>
-        <!-- Twitter -->
-        <a class="tw-ic">
-          <i class="fab fa-twitter white-text mr-4"> </i>
-        </a>
-        <!-- Google +-->
-        <a class="gplus-ic">
-          <i class="fab fa-google-plus-g white-text mr-4"> </i>
-        </a>
-        <!--Linkedin -->
-        <a class="li-ic">
-          <i class="fab fa-linkedin-in white-text mr-4"> </i>
-        </a>
-        <!--Instagram-->  
-        <a class="ins-ic">
-            <i class="fab fa-instagram white-text"> </i>
-          </a>
-
-      </div>
-      <!-- Grid column -->
-
+        <h6 style="font-family:verdana; font-size: 18px; font-weight: 100; text-align: center; padding-top: 12px;">A College Of Engineering Guindy Initiative</h6>
     </div>
-    <!-- Grid row-->
-
-  </div>
 </div>
 
 <!-- Footer Links -->
@@ -68,73 +29,49 @@
   <div class="row mt-3">
 
     <!-- Grid column -->
-    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+    <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-4">
 
       <!-- Content -->
-      <h6 class="text-uppercase font-weight-bold">College name</h6>
+      <h6 class="text-uppercase font-weight-bold">Visit us at</h6>
       <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p style="font-size:20px;">ABC College</p>
+      <p style="font-size:18px;">12, Sardar Patel Rd, Anna University, Guindy, Chennai, Tamil Nadu 600025</p>
 
     </div>
     <!-- Grid column -->
 
     <!-- Grid column -->
-    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
+    <div class="col-md-4 col-lg-4 col-xl-3 mx-auto  mb-4">
       <!-- Links -->
-      <h6 class="text-uppercase font-weight-bold">Products</h6>
+      <h6 class="text-uppercase font-weight-bold">Around the Web</h6>
       <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p>
-        <a href="club1.php">Astro Club</a>
-      </p>
-      <p>
-        <a href="club2.php">Computer Club</a>
-      </p>
-      <p>
-        <a href="club3.php">Photography club</a>
-      </p>
-      <p>
-        <a href="club4.php">Cultural Club</a>
-      </p>
-
+        <p>
+            <i class="fab fa-facebook-f white-text mr-4"> </i>
+      
+       
+      
+          <i class="fab fa-twitter white-text mr-4"> </i>
+    
+      
+        
+          <i class="fab fa-google-plus-g white-text mr-4"> </i>
+        
+  
+      
+          <i class="fab fa-linkedin-in white-text mr-4"> </i>
+        
+        
+            <i class="fab fa-instagram white-text"> </i>
+        </p>
     </div>
     <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-      <!-- Links -->
-      <h6 class="text-uppercase font-weight-bold">Useful links</h6>
-      <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p>
-      <a href="main.php#about1" class="text-dark">Know about us</a>
-      </p>
-      <p>
-      <a href="main.php#clubs" class="text-dark">Clubs</a>
-      </p>
-      <a href="main.php#contact" class="text-dark">Help</a>
-      </p>
-
+    <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-md-0 mb-4">
+    <h6 class="text-uppercase font-weight-bold">Give us a ring</h6>
+    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+    <p>
+        <i class="fas fa-phone mr-3"></i> + 044 2235 8491</p>
+    <p>
+        <i class="fas fa-print mr-3"></i> + 044 2235 8492</p>
     </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-      <!-- Links -->
-      <h6 class="text-uppercase font-weight-bold">Contact</h6>
-      <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-      <p>
-      <i class="fas fa-map-marker-alt mr-3"></i></i>CHENNAI, CH 10012, INDIA</p>
-      <p>
-        <i class="fas fa-envelope mr-3"></i> college@example.com</p>
-      <p>
-        <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-      <p>
-        <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-
-    </div>
-    <!-- Grid column -->
 
   </div>
   <!-- Grid row -->
